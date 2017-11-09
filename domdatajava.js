c10d963118c82dc76da1e2119da46f9b792bbc5b
HaroldDiemef <harold.perez1190@gmail.com>
2017-11-08 22:06:47 -0500

    created datadom

---
+++ domdatajava.js
@@ -0,0 +1,4 @@
+var input = prompt("GIVE ME YOUR NAME NOW!!!");
+document.getElementById('name').innerHTML = "YOUR NAME IS: " + input;
+var changeColor = document.getElementById('name');
+changeColor.style.color = prompt('WHAT COLOR!!!');
