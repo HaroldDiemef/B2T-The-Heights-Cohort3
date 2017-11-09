76a0d27d3f536c787243bcfd118f87f8b2f82d7a
HaroldDiemef <harold.perez1190@gmail.com>
2017-11-08 22:00:25 -0500

    created helloworlddom

---
+++ domjava.js
@@ -0,0 +1,4 @@
+var myPie = document.getElementById('pie');
+myPie.innerHTML = 'Super Heroes';
+myPie.style.color = prompt('pick a color');
+document.getElementById('newtext').innerHTML = 'are better than DC heroes';
